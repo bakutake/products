d280fa7693096b01a190026bfb5314d622faa848
bakutake <loievskyi.slava@gmail.com>
2021-07-26 07:56:07 +0300

    Added top20 products report; Added translations; Small fixes

--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Sum
 
 from sales.models import Order, OrderItem
 from sales.forms import TimeIntervalForm
@@ -41,8 +42,40 @@ class OrdersReportView(View):
 
 
 class TopProductsReportView(View):
+    @query_counter
+    @csrf_exempt
     def get(self, request):
-        pass
+        time_interval_form = TimeIntervalForm(request.GET)
+        if not time_interval_form.is_valid():
+            return render(request, "sales/orders_report.html",
+                          context={"time_interval_form": time_interval_form})
+        start_date = time_interval_form.cleaned_data["start_date"]
+        end_date = time_interval_form.cleaned_data["end_date"]
+
+        order_items = (OrderItem.objects
+                       .filter(order__created_date__range=(start_date, end_date))
+                       .values("product_name")
+                       .annotate(total=Sum("amount"))
+                       .order_by("-total")[:20]
+                       )
+
+        products = self._order_items_to_dict_format(order_items)
+        order_items = (OrderItem.objects
+                       .filter(order__created_date__range=(start_date, end_date),
+                               product_name__in=products)
+                       .select_related("order")
+                       )
+
+        products_result = {product_name: list() for product_name in products}
+        for order_item in order_items:
+            products_result[order_item.product_name].append({
+                "order_number": order_item.order.number,
+                "product_price": order_item.product_price,
+                "created_date": order_item.order.created_date,
+            })
+        return render(request, "sales/top_products_report.html",
+                      context={"products": products_result,
+                               "time_interval_form": time_interval_form})
 
     def _order_items_to_dict_format(self, order_items):
         return {order_item["product_name"]: order_item["total"] for order_item in order_items}
